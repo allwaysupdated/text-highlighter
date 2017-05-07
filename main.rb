7989262e0388f024502e5b3666914072a6a523b1
itggot-Filip-Doracic <jacksonstar3@gmail.com>
2017-05-07 21:28:55 +0200

    yes

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ def read_file(filename:)
     unmarked_text = ""
     f = File.open(filename, "r")
     f.each_line do |line|
-        unmarked_text << line.chomp
+        unmarked_text << line#.chomp
     end
   f.close
   return unmarked_text
@@ -34,24 +34,50 @@ end
 
 def print_highlighted_text(haystack:, indices_list:, needle:)
    progress = 0 #haystack.length
-   index = indices_list.length
-    while progress < haystack.length
-     progress += 1 
-    if progress == haystack[progress]
-        haystack.slice!("ay")
-        p indices_list
+   index = 0
+  # index = indices_list.length
+  while progress < haystack.length
+     
+      if progress == haystack[progress]
+         outputarray = haystack.split(' ')
+         #progress += 1 
+      else
+        outputarray = haystack.split(' ')
+            progress += 1 
+      end
+      while index < outputarray.length
+        
+        if needle == outputarray[index]
+           print outputarray[index].on_green + " "
+           index +=1
+        else
+           print outputarray[index]  +" "
+           index +=1
+      end
+     # puts outputarray
     end
-   
-   end
-   p haystack
+
+  end
+
 end
-#yes
+
 def main
+  if ARGV[0] == "-h"
+    system("cls")
+    puts "This program is case-sensitive"
+  else
     filename = ARGV[0] 
     needle = ARGV[1]
         haystack = read_file(filename:filename)
         system("cls")
         indices_list = indices(haystack:haystack, needle:needle)
         print_highlighted_text(haystack:haystack ,indices_list:indices_list ,needle:needle)
+  end
+    # needle = ARGV[1]
+    #     haystack = read_file(filename:filename)
+    #     system("cls")
+    #     indices_list = indices(haystack:haystack, needle:needle)
+    #     print_highlighted_text(haystack:haystack ,indices_list:indices_list ,needle:needle)
 end
+
 main
